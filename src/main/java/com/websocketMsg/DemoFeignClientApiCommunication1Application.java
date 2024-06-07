4a155e6860eef76786d2aa8d790e9c2ede3fe336
Keyur <keyur.patel0921@gmail.com>
2024-06-07 18:13:57 -0400

    v 1.0

---
+++ src/main/java/com/websocketMsg/DemoFeignClientApiCommunication1Application.java
@@ -0,0 +1,13 @@
+package com.websocketMsg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoFeignClientApiCommunication1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoFeignClientApiCommunication1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/websocketMsg/DemoFeignClientApiCommunication1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.websocketMsg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoFeignClientApiCommunication1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
